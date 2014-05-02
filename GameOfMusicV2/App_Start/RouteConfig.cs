5c671627fe12265929a12c6076c9e17f0bd1617e
Oded Welgreen <oded_wel@hotmail.com>
2014-05-02 17:00:47 +0300

    make the track id part of the route

--- GameOfMusicV2/App_Start/RouteConfig.cs
+++ GameOfMusicV2/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace GameOfMusicV2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Main",
+                url: "{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
