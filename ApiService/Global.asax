6ee01083a1db90bdafe9b256320c1248216c38dd
Oded Welgreen <oded_wel@hotmail.com>
2014-12-12 17:54:40 +0200

    Remote API and Storage projects

--- ApiService/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiService.WebApiApplication" Language="C#" %>
--- GameOfMusicV2/Controllers/HomeController.cs
+++ GameOfMusicV2/Controllers/HomeController.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
-using GameOfMusicV2.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Storage;
 
 namespace GameOfMusicV2.Controllers
 {
@@ -18,18 +7,7 @@ namespace GameOfMusicV2.Controllers
     {
         public async Task<ActionResult> Index(string id = null)
         {
-            if (id != null)
-            {
-                var dc = new TableDataContext<TrackEntity>();
-                var track = await dc.GetSingleEntityAsync(id);
-                if (track != null)
-                {
-                    track.ClientAddress = null;
-                    string json = JsonConvert.SerializeObject(track, Formatting.Indented, 
-                        new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
-                    ViewBag.Track = json;
-                }
-            }
+            
             return View();
         }
 
